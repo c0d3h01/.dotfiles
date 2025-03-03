33c5f8f15bd31e4b2ff8aadced83f4c17c274e23
c0d3h01 <c0d3h01@gmail.com>
2025-03-04 00:06:58 +0530

    networking feat

--- NixOS/networking/default.nix
+++ NixOS/networking/default.nix
@@ -4,12 +4,6 @@
 }:
 
 {
-  # Configure firewall settings
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 1716 ];
-    allowedUDPPorts = [ 1716 ];
-  };
 
   # Enables wireless support via wpa_supplicant (uncomment if needed)
   # networking.wireless.enable = true;
@@ -18,13 +12,33 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Configure firewall settings.
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 1716 ];
+    allowedUDPPorts = [ 1716 ];
+  };
+
+  # Network manager configurations.
   networking.networkmanager = {
     enable = true;
+    
     settings = {
       connection = {
         "wifi.powersave" = 2;
       };
     };
   };
+
+  systemd.network.wait-online.enable = false;
+
+  services.sshd.enable = true;
+
+  # Use systemd-resolved
+  services.resolved.enable = true;
+
+  # Use systemd-networkd
+  systemd.network.enable = true;
+  networking.useNetworkd = true;
 }
 
