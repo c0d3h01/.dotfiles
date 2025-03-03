e14c313c34813bd363c99acccea0fbff350461f9
c0d3h01 <c0d3h01@gmail.com>
2025-03-03 23:59:12 +0530

    removed unneded

--- NixOS/amd/default.nix
+++ NixOS/amd/default.nix
@@ -5,19 +5,10 @@
 , ...
 }:
 {
-  # Make the kernel use the correct driver early.
-  boot.initrd.kernelModules = [ "amdgpu" ];
-
   # Updated option names for OpenGL configuration
   hardware.graphics.extraPackages = with pkgs; [
     rocmPackages.clr.icd
     amdvlk
-  ];
-
-  hardware.graphics.enable32Bit = true;
-
-  hardware.graphics.extraPackages32 = with pkgs; [
-    driversi686Linux.amdvlk
   ];
 
   # verify that OpenCL is correctly setup.
