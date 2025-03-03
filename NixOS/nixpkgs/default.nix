a1d7d67433658a8a98ed4be18e1f941042eebf01
c0d3h01 <c0d3h01@gmail.com>
2025-03-03 22:16:32 +0530

    updated pkgs, removed unnecssary pkg

--- NixOS/nixpkgs/default.nix
+++ NixOS/nixpkgs/default.nix
@@ -1,9 +1,5 @@
 { config, pkgs, ... }:
 
-#pkgs: {
-#  notion-app-enhanced = pkgs.callPackage ./notion-app-enhanced { };
-#}
-
 {
   # -*- Allow unfree softwares -*-
   nixpkgs.config.allowUnfree = true;
@@ -17,26 +13,15 @@
   nix.gc.options = "--delete-older-than 1d";
   nix.settings.auto-optimise-store = true;
 
-  # -*- Gnome boxes settings -*-
+  # -*- Vitrtual Boxes settings -*-
   virtualisation.libvirtd.enable = true;
   users.extraGroups.libvirtd.members = [ "c0d3h01" ];
-
-  # -*- Gsconnect gnome shell extension -*-
-  programs.kdeconnect = {
-    enable = true;
-    package = pkgs.gnomeExtensions.gsconnect;
-  };
-
-  programs.appimage = {
-    enable = true;
-    binfmt = true;
-  };
 
   # Enables nix-ld to run dynamically linked binaries outside the Nix store
   programs.nix-ld.enable = true;
 
   environment.systemPackages = with pkgs; [
-    # -*- Desktop -*-
+    # -*- Desktop GUI Apps -*-
     vscode
     zoom-us
     slack
@@ -44,34 +29,23 @@
     github-desktop
     jetbrains.pycharm-community-bin
     jetbrains.webstorm
-    universal-android-debloater
     postman
     vesktop
     telegram-desktop
     github-desktop
     vscode
-    wine
     slack
     zoom-us
     anydesk
     libreoffice
-    hunspell
-    hunspellDicts.en_US
-    ventoy
     element-desktop
     rhythmbox
     tor-browser
     youtube-music
     transmission_4-gtk
-    docker
-    docker-compose
 
     # -*- Development tools -*-
-    curl
-    wget
-    tree
-    ripgrep
-    nodejs_23
+    nodejs_23 # v23
     zulu23 # java
     postman
     parallel-full # sql
@@ -81,35 +55,34 @@
     android-tools
     universal-android-debloater # uad-ng
     metasploit # msfconsole 
+    nmap
     clang
     gnumake
     cmake
-    python312Packages.ninja
+
+    # Docker cconfigi
+    docker
+    docker-compose
+
+    # Python modules
     python312Packages.pip
-    python312Packages.virtualenv
     python312Packages.django
     python312Packages.flask
     python312Packages.fastapi
     python312Packages.jinja2
 
-    # -*- Terminal utilities -*-
-    eww
+    # -*- Terminal basic utilities -*-
+    wine
+    ventoy
+    curl
+    wget
     xclip
     bat
-    eza
-    fd
-    ripgrep
-    zoxide
-    starship
-    nix-zsh-completions
-    fzf
     tmux
     fastfetch
     inxi
-    nettools
     glances
     shc
-    direnv
     git
     git-lfs
     gh
